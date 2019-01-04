b5ae4c4b4adf481c8b3cc53f13744960368996ad
Eugene Fang <fang.eugene@gmail.com>
2019-01-04 10:04:23 -0800

    Adjust template tabbing

--- src/server.js
+++ src/server.js
@@ -36,30 +36,30 @@ server
     } else {
       res.status(200).send(
         `<!doctype html>
-    <html lang="">
-    <head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta charset="utf-8" />
-        <title>The Blue Alliance</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        ${
-          assets.client.css
-            ? `<link rel="stylesheet" href="${assets.client.css}">`
-            : ''
-        }
-        ${
-          process.env.NODE_ENV === 'production'
-            ? `<script src="${assets.client.js}" defer></script>`
-            : `<script src="${assets.client.js}" defer crossorigin></script>`
-        }
-        <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500">
-        <style id="jss-server-side">${css}</style>
-    </head>
-    <body>
-        <div id="root">${markup}</div>
-    </body>
-</html>`
+          <html lang="">
+          <head>
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta charset="utf-8" />
+            <title>The Blue Alliance</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            ${
+              assets.client.css
+                ? `<link rel="stylesheet" href="${assets.client.css}">`
+                : ''
+            }
+            ${
+              process.env.NODE_ENV === 'production'
+                ? `<script src="${assets.client.js}" defer></script>`
+                : `<script src="${assets.client.js}" defer crossorigin></script>`
+            }
+            <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500">
+            <style id="jss-server-side">${css}</style>
+          </head>
+          <body>
+            <div id="root">${markup}</div>
+          </body>
+        </html>`
       );
     }
   });
