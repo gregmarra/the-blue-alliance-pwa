6b708adfa6b729ecffb01afac3d0b3f7e72cefda
Eugene Fang <fang.eugene@gmail.com>
2019-01-05 03:11:09 -0500

    Disable x-powered-by header

--- src/server.js
+++ src/server.js
@@ -35,6 +35,7 @@ const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
 
 const server = express();
 server
+  .disable('x-powered-by')
   .use(compression())
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
   .get('/*', (req, res) => {
