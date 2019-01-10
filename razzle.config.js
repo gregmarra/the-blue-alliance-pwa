b3c4498acbe3dd8f9fe7afd694cf33eba058bb88
Eugene Fang <fang.eugene@gmail.com>
2019-01-10 15:06:20 -0500

    Ignore Moment.js locale files in webpack

--- razzle.config.js
+++ razzle.config.js
@@ -2,6 +2,11 @@ const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
   modify: (config, { target, dev }, webpack) => {
+    // Ignore all locale files of Moment.js
+    // See: https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
+    config.plugins.push(new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/));
+
+    // Webpack Bundle Analyzer
     if (!dev) {
       config.plugins.push(new BundleAnalyzerPlugin({
         analyzerMode: 'static',
