3909c5f65c8e4a7a0927d81c391693c8ae614592
Eugene Fang <fang.eugene@gmail.com>
2019-01-14 16:41:36 -0500

    Very crude event list search WIP

--- src/pages/EventListPage.js
+++ src/pages/EventListPage.js
@@ -13,6 +13,8 @@ import { getSortedEvents } from '../selectors/EventListPageSelectors'
 
 // Components
 import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import TextField from '@material-ui/core/TextField'
 import Typography from '@material-ui/core/Typography'
 
 // TBA Components
@@ -29,9 +31,49 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 const styles = theme => ({
+  sideNav: {
+    position: 'sticky',
+    top: 56 + theme.spacing.unit*4,
+    [`${theme.breakpoints.up('xs')} and (orientation: landscape)`]: {
+      top: 48 + theme.spacing.unit*4,
+    },
+    [theme.breakpoints.up('sm')]: {
+      top: 64 + theme.spacing.unit*4,
+    },
+  },
+  inputCard: {
+    position: 'sticky',
+    padding: theme.spacing.unit,
+    margin: `0px ${-theme.spacing.unit}px ${theme.spacing.unit}px`,
+    zIndex: 1,
+    // maxWidth: theme.breakpoints.values.sm,
+    top: 56,
+    [`${theme.breakpoints.up('xs')} and (orientation: landscape)`]: {
+      top: 48,
+    },
+    [theme.breakpoints.up('sm')]: {
+      top: 64,
+      margin: `0px auto ${theme.spacing.unit}px`,
+    },
+  },
+  textField: {
+    margin: 0,
+  },
+  list: {
+    // maxWidth: theme.breakpoints.values.sm,
+    margin: '0 auto',
+    paddingBottom: theme.spacing.unit,
+    [theme.breakpoints.up('sm')]: {
+      padding: `0 ${theme.spacing.unit}px ${theme.spacing.unit*3}px`,
+    },
+  },
 })
 
 class EventListPage extends PureComponent {
+  state = {
+    searchText: '',
+  }
+
   static getInitialData({ dispatch, match }) {
     return Promise.all([
       dispatch(fetchYearEvents(match.params.year)),
@@ -42,8 +84,34 @@ class EventListPage extends PureComponent {
     this.props.fetchYearEvents(this.props.year)
   }
 
+  handleSearchFocus = e => {
+    if (e.keyCode === 114 || (e.ctrlKey && e.keyCode === 70)) {
+      e.preventDefault();
+      this.searchRef.current.focus();
+    } else if (e.keyCode === 27) {
+      this.searchRef.current.blur();
+    }
+  }
+
+  handleTextFieldChange = e => {
+    this.setState({searchText: e.target.value});
+  }
+
+  constructor(props) {
+    super(props);
+    this.searchRef = React.createRef();
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleSearchFocus);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleSearchFocus);
+  }
+
   render() {
-    const { year, events } = this.props
+    const { classes, year, events } = this.props
 
     return (
       <TBAPage
@@ -51,13 +119,33 @@ class EventListPage extends PureComponent {
         metaDescription={`${year} season FIRST Robotics Competition events list`}
         refreshFunction={this.refreshFunction}
       >
+        <Typography variant='h4' gutterBottom>{year} <i>FIRST</i> Robotics Competition Events</Typography>
         <Grid container spacing={16}>
           <Grid item xs={12} md={3} lg={2}>
-            TODO: YEAR PICKER & SECTIONS
+            <div className={classes.sideNav}>
+              TODO: YEAR PICKER & SECTIONS
+            </div>
           </Grid>
-          <Grid item xs={12} md={7} lg={8}>
-            <Typography variant='h4' gutterBottom>{year} <i>FIRST</i> Robotics Competition Events</Typography>
-            {events && <EventListCard events={events}/>}
+          <Grid item xs={12} md={9} lg={10}>
+            <Paper className={classes.inputCard} square>
+              <TextField
+                className={classes.textField}
+                inputRef={this.searchRef}
+                label='Search by name or location'
+                fullWidth
+                margin='normal'
+                onChange={this.handleTextFieldChange}
+                // defaultValue={filter}
+              />
+            </Paper>
+            {events && <div className={classes.list}>
+              <EventListCard
+                events={events.filter(event =>
+                  event.name.toLowerCase().includes(this.state.searchText) ||
+                  event.getCityStateCountry().toLowerCase().includes(this.state.searchText)
+                )}
+              />
+            </div>}
           </Grid>
         </Grid>
       </TBAPage>
